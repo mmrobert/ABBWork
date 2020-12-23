4205f942c77df67ce16f69cc24c9b1471a30d73a
realhome <boqian.cheng@yahoo.com>
2020-12-23 10:59:11 -0800

    add datacenter

---
+++ ABBWork/Data Center/DataCenter.swift
@@ -0,0 +1,48 @@
+//
+//  DataCenter.swift
+//  ABBWork
+//
+//  Created by boqian cheng on 2020-12-23.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+import CoreData
+
+class DataCenter {
+    
+    static let shared = DataCenter()
+    
+    @Published var list: [PodcastViewModel] = []
+    private var disposables = Set<AnyCancellable>()
+    
+    private init() {}
+    
+    func fetchPodcasts(term: String) {
+        NetworkService.shared.fetchPodcasts(term: term)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] value in
+                guard let strongSelf = self else { return }
+                switch value {
+                case .failure:
+                    // fetch cached from coredata when no network
+                    strongSelf.list = CoreDataCache.shared.fetchCachedPodcasts(term: term)
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] podcasts in
+                guard let strongSelf = self else { return }
+                strongSelf.list = podcasts.list.map { data in
+                    let pod = PodcastViewModel(dataModel: data)
+                    // cache to coredata (only json data)
+                    // thumb imgae cache using third party
+                    // framework "SDWebImageSwiftUI" buit-in cache
+                    // set up in "AppDelegate.swift" file
+                    CoreDataCache.shared.cachePodCast(viewModel: pod)
+                    return pod
+                }
+            })
+            .store(in: &disposables)
+    }
+}
--- ABBWork/View Models/PodcastListViewModel.swift
+++ ABBWork/View Models/PodcastListViewModel.swift
@@ -8,7 +8,6 @@
 import Foundation
 import Combine
 import SwiftUI
-import CoreData
 
 class PodcastListViewModel: ObservableObject {
     
@@ -22,34 +21,17 @@ class PodcastListViewModel: ObservableObject {
             .removeDuplicates()
             .sink(receiveValue: fetchPodcasts(term:))
             .store(in: &disposables)
+        DataCenter.shared.$list
+            .sink(receiveValue: { [weak self] list in
+                guard let strongSelf = self else { return }
+                strongSelf.list = list
+            })
+            .store(in: &disposables)
     }
     
     func fetchPodcasts(term: String) {
         if term.count > 0 {
-            NetworkService.shared.fetchPodcasts(term: term)
-                .receive(on: DispatchQueue.main)
-                .sink(receiveCompletion: { [weak self] value in
-                    guard let strongSelf = self else { return }
-                    switch value {
-                    case .failure:
-                        // fetch cached from coredata when no network
-                        strongSelf.list = CoreDataCache.shared.fetchCachedPodcasts(term: term)
-                    case .finished:
-                        break
-                    }
-                }, receiveValue: { [weak self] podcasts in
-                    guard let strongSelf = self else { return }
-                    strongSelf.list = podcasts.list.map { data in
-                        let pod = PodcastViewModel(dataModel: data)
-                        // cache to coredata (only json data)
-                        // thumb imgae cache using third party
-                        // framework "SDWebImageSwiftUI" buit-in cache
-                        // set up in "AppDelegate.swift" file
-                        CoreDataCache.shared.cachePodCast(viewModel: pod)
-                        return pod
-                    }
-                })
-                .store(in: &disposables)
+            DataCenter.shared.fetchPodcasts(term: term)
         }
     }
 }
